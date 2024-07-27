20c95397bc841b4fd9506274a73f52d146ba0e3c
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-07-27 17:18:09 +0530

    Task-20 Deploy

--- Task 10/app.js
+++
@@ -1,8 +0,0 @@
-let P = 40000;
-let r = 8;
-let n = 1;
-let t = 3;
-
-const A = P * (1 + r / 100 / n) ** (n * t);
-
-console.log(`The compound interest after ${t} years is: ${A}`);
--- Task 11/app.js
+++
@@ -1,7 +0,0 @@
-let num = 10;
-
-if (num % 2 === 0) {
-	console.log("The number is even.");
-} else {
-	console.log("The number is odd.");
-}
